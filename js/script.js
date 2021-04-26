a589963a2f36fbc1674179625e6d02f942e5968d
lorenzotade <lorenzo.tade@gmail.com>
2021-04-26 17:43:20 +0200

    terminato js manca css

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(function() {
 
+  //inizializzo i tre array per i numeri
   var arrRandom = [];
   var arrUser = [];
-
+  var arrResult = [];
+  //inizializzo la schermata
   reset();
 
   $('#btn-start').click(function(){
-    $('#btn-start').hide();
+    $(this).hide();
+    //con questo ciclo pusho 5 numeri random nell'array apposito e li mostro per 5 secondi a video. Dopodiché mostro input e bottone per l'utente
     while (arrRandom.length < 5) {
       arrRandom.push(getRandomNumber(1, 100));
       printDisplay(arrRandom.join(', '), '#display');
@@ -19,32 +22,46 @@ $(function() {
   }); //end btn-start click
 
   $('#push-num').click(function(){
-    var arrResult = [];
-    arrUser.push($('#input-user').val());
+    //pusho i numeri utente parsati dentro l'array apposito e resetto il campo input
+    arrUser.push(parseInt($('#input-user').val()));
     $('#input-user').val('');
+    //quando nell'array ci sono 5 numeri scompare input e bottoni, il computer "calcola" e parte il ciclo for
     if (arrUser.length === 5) {
+      $('#input-user').hide();
+      $(this).hide();
       printDisplay('Calcolo in corso...', '#display');
-      for (var i = 0; i < arrRandom.length; i++) {
+      //con questo ciclo controllo se dei numeri inseriti dall'utente ce ne di presenti all'interno di quelli random; se così il numero viene pushato nell'array del risultato
+      for (var i = 0; i < arrUser.length; i++) {
         if(arrRandom.includes(arrUser[i])) {
           arrResult.push(arrUser[i]);
         }
-        console.log(arrResult);
       }
+      //dopo tre secondi viene mostrato l'output in base al risultato ed il bottone per ricominciare il gioco
       setTimeout(function(){
-        printDisplay('Hai indovinato ' + arrResult.length + ' numeri: ' + arrResult.join(', '), '#display');
+        if (arrResult.length == 0) {
+          printDisplay('Hai perso, nessun numero indovinato!', '#display');
+        } else {
+          printDisplay('Hai indovinato ' + arrResult.length + ' numeri: ' + arrResult.join(', '), '#display');
+        }
+        $('#restart').show();
       }, 3000);
   }
   }); //end push-num click
-  
 
+  $('#restart').click(function(){
+    reset();
+  }); //end restart click
+
+  /* FUNZIONI */
 
   //funzione per ritornare alla situazione di default iniziale
   function reset() {
     $('#display').text('Sei pronto a giocare?');
+    $('#btn-start').show();
     $('#input-user').hide();
     $('#push-num').hide();
+    $('#restart').hide();
   }
-
   //funzione che mi genera un numero massimo tra min e max decisi dall'utente
   function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -53,6 +70,5 @@ $(function() {
   function printDisplay(output, target) {
     $(target).text(output);
   }
-
-
+  
 }); //end document ready
